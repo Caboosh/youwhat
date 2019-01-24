41011d45a4a6b38d168ee8dfe0142cdc1292e05b
Cameron Miller <cambomiller@googlemail.com>
2019-01-24 12:23:51 +0000

    Updated Run.bat to actually be a batch file
    Batch files are not Bash files for fuck sake XD

--- run.bat
+++ run.bat
@@ -1,2 +1,8 @@
-#!/bin/bash
-echo "That's how mafia works"
+@echo off
+if not exist mafiaworks.txt (
+echo This is not how Mafia Works.
+) else (
+   echo This is how mafia works:
+   echo.
+   dir /b mafiaworks.txt
+   )
